cba4a4fe817018847996ba6e6439be59be1b0aa3
0WN463 <>
2023-10-09 01:35:03 +0800

    Add link and hover effect to preview

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 import Grid from "components/grid_preview";
+import Link from "next/link";
 
 export default async function Home() {
-  const supabaseUrl = process.env.SUPABASE_URL; 
+  const supabaseUrl = process.env.SUPABASE_URL;
   const supabaseKey = process.env.SUPABASE_KEY;
   const supabase = createClient(supabaseUrl, supabaseKey);
 
@@ -15,11 +16,16 @@ export default async function Home() {
       <h1 className="p-4 text-lg flex items-center justify-center">
         Choose a puzzle
       </h1>
-      <div className="flex flex-wrap items-center justify-between p-24 gap-4">
-        {sudoku_puzzles.map((p) => (
-          <div key={p.id}>
+      <div className="flex flex-wrap items-center justify-center md:justify-start p-24 gap-8">
+        {sudoku_puzzles.map((p, index) => (
+          <Link
+            href={"/" + p.id}
+            key={p.id}
+            className="border rounded hover:bg-sky-700"
+          >
             <Grid puzzle={p.puzzle} />
-          </div>
+            <span className="flex justify-center">Puzzle #{index}</span>
+          </Link>
         ))}
       </div>
     </main>
--- components/grid_preview.tsx
+++ components/grid_preview.tsx
@@ -1,7 +1,7 @@
 const Grid = ({ puzzle }: { puzzle: string }) => (
-  <div className="h-64 w-64 bg-black grid-cols-9 grid-rows-9 grid auto-rows-fr auto-cols-fr> hover:ring-1">
+  <div className="h-64 w-64 bg-black grid-cols-9 grid-rows-9 grid auto-rows-fr auto-cols-fr">
     {[...puzzle].map((i, index) => (
-      <div key={index} className="border text-center">
+      <div key={index} className="border text-center pointer-events-none">
         {i === "." ? " " : i}
       </div>
     ))}
