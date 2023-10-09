f0298506c12c7832c01b9e274fc8c3eead2fd38a
0WN463 <>
2023-10-10 01:34:29 +0800

    Add input validation to cells

--- components/grid_playzone.tsx
+++ components/grid_playzone.tsx
@@ -2,27 +2,46 @@
 
 import React, { useState } from "react";
 
+type Cell = {
+  fixed: boolean;
+  value?: number;
+};
+
 const Grid = ({ puzzle }: { puzzle: string }) => {
-  const [state, setState] = useState(
+  const [state, setState] = useState<Cell>(
     [...puzzle].map((cell) =>
-      cell === "." ? { fixed: false, value: "" } : { fixed: true, value: cell },
+      cell === "." ? { fixed: false } : { fixed: true, value: parseInt(cell) },
     ),
   );
+
+  const onChange = (e, index: number) => {
+    if (e.target.value === "") {
+      setState(state.with(index, { fixed: false }));
+      return;
+    }
+
+    const num = parseInt(e.target.value);
+    if (isNaN(num) || num <= 0 || num > 9) {
+      e.target.setCustomValidity("number out of range");
+      return;
+    }
+
+    e.target.setCustomValidity("");
+    setState(state.with(index, { fixed: false, value: num }));
+  };
 
   return (
     <div className="h-64 w-64 bg-black grid-cols-9 grid-rows-9 grid auto-rows-fr auto-cols-fr">
       {state.map(({ fixed, value }, index) => (
         <input
-          className="border text-center bg-white text-green-900 disabled:bg-slate-100 disabled:text-black"
+          className="border text-center bg-white text-green-900 invalid:text-red-900 disabled:bg-slate-100 disabled:text-black"
           maxLength="1"
           key={index}
           min="1"
           max="9"
           disabled={fixed}
-          onChange={(e) =>
-            setState(state.with(index, { fixed: false, value: e.target.value }))
-          }
-          value={value}
+          onChange={(e) => onChange(e, index)}
+          value={value ?? ""}
         />
       ))}
     </div>
